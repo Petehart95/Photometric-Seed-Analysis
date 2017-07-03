eba8c6f9c823e48f86b07114d8b5cb95589e3445
Peter Hart <12421031@students.lincoln.ac.uk>
2017-07-03 18:12:23 +0100

    Added additional directories

---
+++ Artefact/PhotometricSeedAnalysis.m
@@ -0,0 +1,9 @@
+%UROS Project (July 2017) - Photometric Seed Analysis
+%Peter Leonard Hart - HAR12421031
+%Staff Supervisor: Wenting Duan
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% -- SCRIPT START -- %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%end of script
