66f3d28fb58e5fe24b8a1321d17cc58f73640f71
Peter Hart <12421031@students.lincoln.ac.uk>
2017-07-10 00:20:51 +0100

    Implemented image segmentation via k-means clustering

--- Artefact/PhotometricSeedAnalysis.m
+++ Artefact/PhotometricSeedAnalysis.m
@@ -1,9 +1,52 @@
 %UROS Project (July 2017) - Photometric Seed Analysis
 %Peter Leonard Hart - HAR12421031
 %Staff Supervisor: Wenting Duan
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% -- SCRIPT START -- %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close all; clc; clear;
 
+% FilterSpec = '.jpg';
+% [FileName,PathName,FilterIndex] = uigetfile(FilterSpec);
+% selectedFile = strcat(PathName,FileName);
+selectedFile = 'C:\Users\Peter\Documents\GitHub\UROS_PhotometricSeedAnalysisProject\Artefact\Images\Capture_00012.jpg';
+im = imread(selectedFile);
 
+imStruct = size(im(:,:,:));  % Declaring variables to store key properties of the image
+imRows = (imStruct(1)); 
+imColumns = (imStruct(2)); 
+imChannels = (imStruct(3));
+
+imGrey = rgb2gray(im); % Greyscale conversion formula: 0.2989 * R + 0.5870 * G + 0.1140 * B 
+imBin = zeros(imRows,imColumns);
+imMedian = zeros(imRows,imColumns);
+
+%imBin = im2bw(imGrey,graythresh(imGrey));
+%imBinC = ~imBin;
+%D = bwdist(imBinC);
+%L = watershed(-D);
+%w = L == 0;
+%g2 = imBin & ~w;
+
+cform = makecform('srgb2lab');
+lab_he = applycform(im,cform);
+
+ab = double(lab_he(:,:,2:3));
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+
+nColors = 3; %LAB
+[cluster_idx, cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+segmented_images = cell(1,3);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+
+for k = 1:nColors
+    color = im;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+end
+
+figure;
+imshow(segmented_images{2}), title('objects in cluster 2');
+figure;
+imshow(im);
 %end of script
