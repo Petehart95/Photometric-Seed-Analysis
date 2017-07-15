028bc082cb9f83e48fcad4fc784c6f018e3f7131
Peter Hart <12421031@students.lincoln.ac.uk>
2017-07-15 13:00:52 +0100

    Experimenting with morphological operations

--- Artefact/PhotometricSeedAnalysis.m
+++ Artefact/PhotometricSeedAnalysis.m
@@ -1,27 +1,38 @@
-%UROS Project (July 2017) - Photometric Seed Analysis
-%Peter Leonard Hart - HAR12421031
-%Staff Supervisor: Wenting Duan
+% UROS Project (July 2017) - Photometric Seed Analysis
+% Peter Leonard Hart - HAR12421031
+% Staff Supervisor: Wenting Duan
 
 % LOAD IMAGE
 close all; clc; clear; % Reset
-selectedFile = 'C:\Users\Peter\Documents\GitHub\UROS_PhotometricSeedAnalysisProject\Artefact\Images\Capture_00016.jpg';
+selectedFile = 'C:\Users\Peter\Documents\GitHub\UROS_PhotometricSeedAnalysisProject\Artefact\Images\Capture_00535.jpg';
 im = imread(selectedFile); % Gather input
 imStruct = size(im(:,:,:));  % Store the structural data about the image
 imRows = (imStruct(1)); imColumns = (imStruct(2)); imChannels = (imStruct(3)); % Store structure data into separate variables (for clarity)
 
 % IMAGE ENHANCEMENT
-imGrey = rgb2gray(im); % Greyscale conversion formula: 0.2989 * R + 0.5870 * G + 0.1140 * B 
+im2 = imtophat(im,strel('disk',10));
+imGrey = rgb2gray(im2); % Greyscale conversion formula: 0.2989 * R + 0.5870 * G + 0.1140 * B 
+imGrey = imadjust(imGrey);
 imHist = histeq(imGrey); %histogram equalisation
 
-% SEGMENTATION
-% WATERSHED
-% imBin = im2bw(imGrey,graythresh(imGrey));
-% imBinC = ~imBin;
-% D = bwdist(imBinC);
-% L = watershed(-D);
-% w = L == 0;
-% g2 = imBin & ~w;
 
+
+% SEGMENTATION
+
+% WATERSHED
+
+imBin = im2bw(imGrey,graythresh(imGrey));
+imBinC = ~imBin;
+imBinED = bwdist(imBinC,'euclidean'); 
+L = watershed(-imBinED); %label matrix
+w = L == 0;
+g2 = imBin & ~w;
+im2 = imdilate(imBinED,strel('disk',12));
+im2 = imerode(im2,strel('disk',12));
+figure;
+imshow(g2);
+figure;
+imshow(im2);
 % K-MEANS CLUSTERING
 % cform = makecform('srgb2lab');
 % lab_he = applycform(im,cform);
@@ -41,7 +52,5 @@ imHist = histeq(imGrey); %histogram equalisation
 %     color = im;
 %     color(rgb_label ~= k) = 0;
 %     segmented_images{k} = color;
-% end
-
 
 %end of script
